a06045f3e431978087e22e639e6c3ec277f7931f
Paweł Grzesiewicz <p.grzesiewicz@gmail.com>
2024-01-30 21:11:12 +0100

    added full responsive fotter section plas newsletter

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const FooterOverlay = () => {
   return (
-    <div>
-      
+    <div className="app__footerOverlay">
+      <div className="app__footerOverlay-black" />
+      <div className="app__footerOverlay-img app__bg" />
     </div>
+
   )
 }
-
+ 
 export default FooterOverlay
